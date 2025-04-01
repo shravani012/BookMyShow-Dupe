fc0371fd72de1d63acf72b8d008bc7cd635bcbc8
shravani012 <113746749+shravani012@users.noreply.github.com>
2025-04-01 12:11:42 +0530

    Delete Backend directory

--- Backend/models/Booking.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const BookingSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
-    paymentStatus: { type: String, default: "Pending" }
-});
-
-module.exports = mongoose.model("Booking", BookingSchema);
